5e5c376ba681ac549405dc0742e42d937b889c77
yasser <yasser.fadhl@maxnetds.com>
2023-05-14 15:06:59 +0200

    Error Handling

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
+require("colors");
 const express = require("express");
+const connectDB = require("./config/db");
+const bootCamps = require("./routes/bootcamps");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv");
-
 dotenv.config({ path: "./config/config.env" });
-
 const app = express();
 
-const port = process.env.PORT || 8080;
+connectDB();
 
-app.listen(
+//Body parser
+app.use(express.json());
+app.use("/api/v1/bootcamps", bootCamps);
+
+app.use(errorHandler);
+const port = process.env.PORT || 8080;
+const server = app.listen(
   port,
   console.log(
-    `Server is running on port ${port} in ${process.env.NODE_ENV} mode`
+    `Server is running on port ${port} in ${process.env.NODE_ENV} mode`.blue
+      .bold
   )
 );
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
