89135e763c103790dabf1465482d5f826fa93446
yasser <yasser.fadhl@maxnetds.com>
2023-05-14 15:05:26 +0200

    middlewares & utils

---
+++ middleware/errorHandler.js
@@ -0,0 +1,26 @@
+const ErrorResponse = require("../utils/errorResponse");
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+
+  error.message = err.message;
+
+  console.log(err.errors);
+  // console.log(err.name.bold);
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorResponse(message, 400);
+  }
+  if (err.name === "CastError") {
+    const message = `Boocamp with ID ${err.value} is not found`;
+    error = new Error(message, 404);
+  }
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || "Server Error",
+  });
+};
+module.exports = errorHandler;
